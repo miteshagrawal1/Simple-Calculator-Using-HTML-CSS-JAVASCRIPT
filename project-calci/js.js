5ab74b504a12ad0dfb89385b5d6f67b05df557e0
miteshagrawal1 <115792308+miteshagrawal1@users.noreply.github.com>
2023-06-17 13:27:23 +0530

    Add files via upload

---
+++ project-calci/js.js
@@ -0,0 +1,8 @@
+let screen = document.getElementById('screen');
+// buttons = document.querySelectorAll('button');
+// for (item of buttons){
+//     item.addEventListner('click' , (e)=>{
+//         buttonText = e.target.innerText;
+//         console.log('Button text is' , buttonText);
+//     })
+// }
